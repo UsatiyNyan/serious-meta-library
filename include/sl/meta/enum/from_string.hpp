f91f74e91cbe6c3f6d967430d9646a44aad91bd7
Usatiy Nyan <kirillarionov123456@gmail.com>
2024-03-21 02:10:48 +0100

    enum from string

--- include/sl/meta/enum/from_string.hpp
+++ include/sl/meta/enum/from_string.hpp
@@ -7,6 +7,8 @@
 
 #include "to_string.hpp"
 
+#include "sl/meta/match/match_map.hpp"
+
 #include <string_view>
 #include <type_traits>
 
@@ -28,14 +30,24 @@ concept EnumConvertibleFromString = //
 } // namespace detail
 
 template <detail::EnumConvertibleFromString Enum>
-constexpr Enum enum_from_str(std::string_view str) {
-    for (Enum e = Enum{}; e != Enum::ENUM_END;
-         e = static_cast<Enum>(static_cast<std::underlying_type_t<Enum>>(e) + 1)) {
-        if (enum_to_str(e) == str) {
-            return e;
+constexpr auto make_enum_match_map() {
+    return make_match_map<std::string_view, Enum>([](auto match) {
+        for (Enum e = Enum{}; e != Enum::ENUM_END;
+             e = static_cast<Enum>(static_cast<std::underlying_type_t<Enum>>(e) + 1)) {
+            constexpr std::string_view e_str = enum_to_str(e);
+            if (e_str.empty()) {
+                continue;
+            }
+            match = match.case_(e_str, e);
         }
-    }
-    return Enum::ENUM_END;
+        return match;
+    });
+}
+
+template <detail::EnumConvertibleFromString Enum>
+constexpr Enum enum_from_str(std::string_view str) {
+    constexpr auto m = make_enum_match_map<Enum>();
+    return m[str].value_or(Enum::ENUM_END);
 }
 
 } // namespace sl::meta

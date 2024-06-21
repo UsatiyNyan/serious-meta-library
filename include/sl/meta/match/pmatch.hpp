e51239bc70722a6a14ddc79507ab88fc190b56f1
UsatiyNyan <us4tiyny4n@gmail.com>
2024-06-21 14:17:29 +0200

    improve pmatch

--- include/sl/meta/match/pmatch.hpp
+++ include/sl/meta/match/pmatch.hpp
@@ -8,8 +8,6 @@
 #include "sl/meta/match/overloaded.hpp"
 #include "sl/meta/traits/treat_as.hpp"
 
-#include <optional>
-#include <type_traits>
 #include <variant>
 
 #include <tl/optional.hpp>
@@ -22,32 +20,32 @@ public:
     constexpr explicit pmatch(Fs&&... fs) : overloaded_{ std::forward<Fs>(fs)... } {}
 
     template <treat_as_ptr T>
-    constexpr auto operator()(T&& v) && {
+    constexpr auto operator()(T&& v) const {
         if (v == nullptr) {
-            return std::move(overloaded_)();
+            return overloaded_();
         }
-        return std::move(overloaded_)(deref<T>(std::forward<T>(v)));
+        return overloaded_(deref<T>(std::forward<T>(v)));
     }
 
     template <treat_as_optional T>
-    constexpr auto operator()(T&& v) && {
+    constexpr auto operator()(T&& v) const {
         if (!v.has_value()) {
-            return std::move(overloaded_)();
+            return overloaded_();
         }
-        return std::move(overloaded_)(std::forward<T>(v).value());
+        return overloaded_(std::forward<T>(v).value());
     }
 
     template <treat_as_expected T>
-    constexpr auto operator()(T&& v) && {
+    constexpr auto operator()(T&& v) const {
         if (!v.has_value()) {
-            return std::move(overloaded_)(std::forward<T>(v).error());
+            return overloaded_(std::forward<T>(v).error());
         }
-        return std::move(overloaded_)(std::forward<T>(v).value());
+        return overloaded_(std::forward<T>(v).value());
     }
 
     template <treat_as_variant T>
-    constexpr auto operator()(T&& v) && {
-        return std::visit(std::move(overloaded_), std::forward<T>(v));
+    constexpr auto operator()(T&& v) const {
+        return std::visit(overloaded_, std::forward<T>(v));
     }
 
 private:
@@ -55,8 +53,8 @@ private:
 };
 
 template <typename T, typename... Fs>
-constexpr auto operator|(T&& v, pmatch<Fs...>&& pm) {
-    return std::move(pm)(std::forward<T>(v));
+constexpr auto operator|(T&& v, const pmatch<Fs...>& pm) {
+    return pm(std::forward<T>(v));
 }
 
 } // namespace sl::meta

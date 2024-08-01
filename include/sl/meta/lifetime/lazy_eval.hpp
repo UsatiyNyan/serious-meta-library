000f9e9cffcad844d89d6f1ee015f2bd33ba3234
UsatiyNyan <us4tiyny4n@gmail.com>
2024-08-02 01:34:33 +0200

    lazy_eval: identity

--- include/sl/meta/lifetime/lazy_eval.hpp
+++ include/sl/meta/lifetime/lazy_eval.hpp
@@ -16,10 +16,23 @@ class lazy_eval : public unique {
 public:
     constexpr explicit lazy_eval(F f) : f_{ std::move(f) } {}
 
-    [[nodiscard]] constexpr operator T() && { return f_(); }
+    [[nodiscard]] constexpr operator T() && { return std::move(f_)(); }
+    [[nodiscard]] constexpr T operator()() && { return std::move(f_)(); }
 
 private:
     F f_;
+};
+
+template <typename T>
+class identity_eval : public unique {
+public:
+    constexpr explicit identity_eval(T value) : value_{ std::move(value) } {}
+
+    [[nodiscard]] constexpr operator T() && { return std::move(value_); }
+    [[nodiscard]] constexpr T operator()() && { return std::move(value_); }
+
+private:
+    T value_;
 };
 
 } // namespace sl::meta

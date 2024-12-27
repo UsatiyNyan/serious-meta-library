52f80bba39d3c472fa8b1a4320dd0ca6c162f421
UsatiyNyan <us4tiyny4n@gmail.com>
2024-12-28 00:31:29 +0100

    type: head, at

--- include/sl/meta/type/list.hpp
+++ include/sl/meta/type/list.hpp
@@ -20,10 +20,26 @@ struct head<list<T, Ts...>> {
     using type = T;
 };
 
+template <std::size_t, typename>
+struct at;
+
+template <typename T, typename... Ts>
+struct at<0, list<T, Ts...>> {
+    using type = T;
+};
+
+template <std::size_t Idx, typename T, typename... Ts>
+struct at<Idx, list<T, Ts...>> {
+    using type = typename at<Idx - 1, list<Ts...>>::type;
+};
+
 } // namespace detail
 
 template <typename... Ts>
-using head_t = typename detail::head<Ts...>::type;
+using head_t = typename detail::head<detail::list<Ts...>>::type;
+
+template <std::size_t Idx, typename... Ts>
+using at_t = typename detail::at<Idx, detail::list<Ts...>>::type;
 
 template <typename T, typename... Ts>
 constexpr bool are_same_v = (std::is_same_v<T, Ts> && ... && true);
--- test/src/type_test.cpp
+++ test/src/type_test.cpp
@@ -8,6 +8,18 @@
 
 namespace sl::meta {
 
+TEST(type, head) {
+    static_assert(std::is_same_v<int, type::head_t<int>>);
+    static_assert(std::is_same_v<int, type::head_t<int, char>>);
+    static_assert(std::is_same_v<char, type::head_t<char, int, char>>);
+}
+
+TEST(type, at) {
+    static_assert(std::is_same_v<int, type::at_t<0, int>>);
+    static_assert(std::is_same_v<void, type::at_t<3, int, char, std::string, void>>);
+    static_assert(std::is_same_v<std::string, type::at_t<2, int, char, std::string, void>>);
+}
+
 TEST(type, areSame) {
     static_assert(type::are_same_v<int>);
     static_assert(type::are_same_v<int, int, int>);

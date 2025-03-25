701175e833298764443fd85866c5f80490cdd596
UsatiyNyan <us4tiyny4n@gmail.com>
2025-03-25 14:33:28 +0100

    func: undefined: runtime -> compile-time error

--- include/sl/meta/func/undefined.hpp
+++ include/sl/meta/func/undefined.hpp
@@ -4,12 +4,16 @@
 
 #pragma once
 
-#include <libassert/assert.hpp>
+#include <type_traits>
 
 namespace sl::meta {
 
 struct undefined {
-    [[noreturn]] undefined() { PANIC(); }
+    template <typename T>
+    constexpr operator T() const {
+        static_assert(!std::is_same_v<T, T>, "trying to evaluate undefined");
+        return {};
+    }
 };
 
 } // namespace sl::meta
